df2d5cfc2fe1347b2165d6d354303dad92f77fd5
kapiltiwari1 <Kapil.Tiwari@rl-institut.de>
2021-09-21 15:11:03 +0200

    updated

--- src/pre_design_wastewater_biogas.py
+++ src/pre_design_wastewater_biogas.py
@@ -1,12 +1,13 @@
+# digester file located in src >>> components
 import pandas as pd
 
 from digester_demand import heat_calculation
 from digester_demand import electricity_calculation
 from digester import Digester
 
-inpdf = pd.read_csv(r'temperature1.csv')
-outdf = pd.read_csv(r'basic_example_copy_1.csv')
-flow = outdf["wastewater"].mean()
+
+inpdf = pd.read_csv(r'ww_biogas_tibnine_raw.csv')
+flow = inpdf["wastewater"].mean()
 print(flow)
 
 surface_area = Digester(22, flow)
@@ -16,12 +17,12 @@ print('surface_area_total_overground: ', round(area, 2))
 for i, r in inpdf.iterrows():
     heat_demand = heat_calculation(temp_ambient=r['temperature'], heat_transfer_coefficient=0.6,
                                    temp_digester=35, surface_area_total_overground= area)
-    outdf.loc[i, "heat_demand_digester"] = heat_demand.compute()
+    inpdf.loc[i, "heat_demand_digester"] = heat_demand.compute()
 
-outdf.to_csv("test_file.csv", index=False)
+inpdf.to_csv("ww_biogas_tibnine_proceed.csv", index=False)
 
-for i, r in outdf.iterrows():
+for i, r in inpdf.iterrows():
     electricity_demand = electricity_calculation(wastewater=r['wastewater'])
-    outdf.loc[i, "electricity_demand_digester"] = electricity_demand.compute()
+    inpdf.loc[i, "electricity_demand_digester"] = electricity_demand.compute()
 
-outdf.to_csv("test_file.csv", index=False)
+inpdf.to_csv("ww_biogas_tibnine_proceed.csv", index=False)
