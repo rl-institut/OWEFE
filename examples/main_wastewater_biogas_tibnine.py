1351f8d3dba415765907ffec49cca9a01bcea7cc
Julian Barinton <julianfleischmann@gmail.com>
2021-11-23 17:13:38 +0000

    integrate pre_design into main

--- examples/main_wastewater_biogas_tibnine.py
+++ examples/main_wastewater_biogas_tibnine.py
@@ -66,7 +66,7 @@ __copyright__ = "oemof developer group"
 __license__ = "GPLv3"
 
 # *********************************************************************************************
-# ******************* PART 1 - Define and optimise the iWEFEs *********************************
+# ******************* PART 1 - Define the iWEFEs **********************************************
 # *********************************************************************************************
 
 # *********************************************************************************************
@@ -76,7 +76,6 @@ __license__ = "GPLv3"
 # Default logger of oemof
 
 import csv
-import math
 from oemof.tools import logger
 from oemof import solph
 
@@ -86,32 +85,34 @@ import pandas as pd
 import pprint as pp
 import numpy as np
 
+
 # changing cwd to use the function from the main file
 os.chdir("../src/")
 from src.components.digester import Digester
 from src.components.constructedwetlands import Constructed_wetlands
+from digester_demand import HeatCalculation
+from digester_demand import ElectricityCalculation
 
 os.chdir("../examples/")
-from examples import pre_design_wastewater_biogas
+
 # *********************************************************************************************
 # inputs
 # *********************************************************************************************
-# Read input data file after running pre-design
-data = pd.read_csv(r"ww_biogas_tibnine_proceed.csv")
+# Read input data file after
+data = pd.read_csv(r"ww_biogas_tibnine_raw.csv")
 # Units of raw data file: wastewater/dewatered_sludge [kg/h], heat demand [kWh/h],
 # electricity demand [kWh/h], water demand [m³]
-design_mass_flowrate = data["wastewater"].max()
-print(f'Mass flow rate {design_mass_flowrate}')
+
 # *********************************************************************************************
 # case study specific iWEFEs characteristics
 # *********************************************************************************************
-
 # Digester operation parameters
 # temperature inside of the digester = 35 degree Celsius (source: final year project, 2021, Beirut Arab University)
-retention_time = 22 # retention time set to 22 days (source: final year project, 2021, Beirut Arab University)
+retention_time = 22  # retention time set to 22 days (source: final year project, 2021, Beirut Arab University)
 # Characteristic of input sludge
 # incoming organic matter flow
 design_mass_flowrate = data["wastewater"].max()  # [kg/h]
+print(f'Mass flow rate {design_mass_flowrate}')
 sludge_density = 997  # kg/m3 (source: final year project, 2021, Beirut Arab University)
 sludge_specific_gravity = 1.02  # unitless, (source: final year project, 2021, Beirut Arab University)
 heat_capacity_sludge = 4200  # J/kg°C heat capacity dewatered sludge assumed to be same as water
@@ -120,6 +121,24 @@ yield_factor = 9.3  # (source: final year project, 2021, Beirut Arab University)
 # dry solid concentration = 20% of wet feedstock sludge (source: final year project, 2021, Beirut Arab University)
 # volatile solid concentration = 80% of dry solid (source: final year project, 2021, Beirut Arab University)
 
+digester_design = Digester(retention_time, design_mass_flowrate, sludge_density, sludge_specific_gravity, yield_factor)
+diameter, volume, bg_prod, surface_area_total, filled_up_volume, organic_loading_rate, volumetric_flowrate = digester_design.compute()
+print('surface_area_total: ', round(surface_area_total, 2))
+
+for i, r in data.iterrows():
+    heat_demand = HeatCalculation(temp_ambient=r['temperature'], heat_transfer_coefficient=0.6,
+                                  temp_digester=35, surface_area=surface_area_total,
+                                  heat_capacity=heat_capacity_sludge, om_flow=design_mass_flowrate)
+    data.loc[i, "heat_demand_digester"] = heat_demand.compute()
+
+data.to_csv("ww_biogas_tibnine_proceed.csv", index=False)
+
+for i, r in data.iterrows():
+    electricity_demand = ElectricityCalculation(om_flow=r['wastewater'], filled_up_volume=filled_up_volume)
+    data.loc[i, "electricity_demand_digester"] = electricity_demand.compute()
+
+data.to_csv("ww_biogas_tibnine_proceed.csv", index=False)
+
 try:
     import matplotlib.pyplot as plt
 except ImportError:
@@ -127,7 +146,7 @@ except ImportError:
 
 solver = "cbc"
 debug = True  # Set number_of_timesteps to 3 to get a readable lp-file.
-number_of_time_steps = 24 * 7 * 52  # 24 hour * 7 days * 52 weeks
+number_of_time_steps = 24 * 365  # 24 hour * 365 days
 solver_verbose = False  # show/hide solver output
 
 # initiate the logger (see the API docs for more information)
